054783a6f770fbf60fa9ee8248b9287f6981cda7
pasxn <pasansanjula.android@gmail.com>
2023-07-09 15:22:21 +0530

    test pipeline

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd V3DLib
-make --file=make_kernels QPU=0 DEBUG=1 $1
+make QPU=0 DEBUG=1 $1
--- test.sh
+++ test.sh
@@ -2,13 +2,26 @@
 
 ./clonebuildv3d.sh
 
+sleep 1
+
 directory="./kernels"
 
 cd "$directory" || exit 1
 cpp_files=$(find . -maxdepth 1 -type f -name "*.cpp")
 cd ..
 
+printf "\n\n================================ Building ================================\n\n"
 for file in $cpp_files; do
   filename=$(basename "$file" .cpp)
   ./make.sh $filename
+done
+
+sleep 1
+
+cd target/emu-debug/bin
+
+printf "\n\n================================ Running Tests ================================\n\n"
+for file in $cpp_files; do
+  filename=$(basename "$file" .cpp)
+  ./$filename
 done
